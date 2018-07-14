1f32149a3bc52e6bca1f62d27b0cc33ff7d41dc6
Sette <brunosilvasette@gmail.com>
2018-07-14 18:44:52 -0400

    Primeiros testes

--- intro.py
+++ intro.py
@@ -43,4 +43,56 @@ def find_analogies(w1,w2,w3):
 
 ## Rápido
 def find_analogies(w1,w2,w3):
-	
+	for w in (w1,w2,w3):
+		if w not in word2vec:
+			print("%s not in dictionary" % w)
+			return
+
+	king = word2vec[w1]
+	man = word2vec[w2]
+	woman = word2vec[w3]
+	v0 = king - man + woman
+
+	distances = pairwise_distances(v0.reshape(1,D), embedding,metric=metric).reshape(V)
+	idx = distances.argwin()
+	best_word = idx2word[idx]
+
+	print(w1, "-", w2, "=",best_word,"-",w3)
+
+
+def nearest_neighbors(w,n=5):
+	if w not in word2vec:
+		print("%s not in dictionary" % w)
+		return
+
+	v = word2vec[w]
+	distances = pairwise_distances(v0.reshape(1,D), embedding,metric=metric).reshape(V)
+	idx = distances.argsort()[1:n+1]
+	print("neighbords of : %s" % w)
+	for idx in idxs:
+		print("\t%s" % idx2word[idx])
+
+
+
+
+# load pre=treined word vectors
+
+print("Loading word vectors ...")
+word2vec = {}
+embedding = []
+idx2word = []
+with open("/home/bruno/w2v/glove.6B") as f:
+	for line in f:
+		values = line.split()
+		word = values[0]
+		vec = np.asarray(values[1:],dtype='float32')
+		word2vec[word] = vec
+		embedding.append(vec)
+		idx2word.append(word)
+
+print("Found %s word vector." % len(word2vec) )	
+embedding = np.array(embedding)
+V,D = embedding.shape
+
+
+find_analogies('king','man','woman')
